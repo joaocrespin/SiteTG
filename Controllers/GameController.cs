eb6f91e6ef59e5ab258d2298eaf88ccd4e88aaf4
João Guilherme <joaorato58@gmail.com>
2026-04-16 23:19:02 -0300

    feat: add GameController for download functionality

---
+++ Controllers/GameController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GameController : ControllerBase
+{
+    private readonly IWebHostEnvironment _env;
+
+    public GameController(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
+    [HttpGet("download")]
+    public IActionResult Download()
+    {
+        var filePath = Path.Combine(_env.WebRootPath, "downloads", "jogo.zip");
+
+        if (!System.IO.File.Exists(filePath))
+            return NotFound("Arquivo não encontrado.");
+
+        var fileBytes = System.IO.File.ReadAllBytes(filePath);
+        return File(fileBytes, "application/zip", "jogo.zip");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
